1be26806722e74507b206b0495ccfdb52bd8d08f
Julien <julien.drochon@gmail.com>
2018-10-26 21:32:53 +0200

    Initial commit

---
+++ 2017-2018_Aleksandra_chatbot_dummy_mirror/sketch.js
@@ -0,0 +1,51 @@
+// A2Z F17
+// Daniel Shiffman
+// http://shiffman.net/a2z
+// https://github.com/shiffman/A2Z-F17
+
+var speech = new p5.Speech();
+var reply;
+
+function setup() {
+  noCanvas();
+
+
+
+  // Create a RiveScript bot
+  var bot = new RiveScript();
+
+  // Load the bot files (this can be more than one)
+  var files = ['brain/knockknock.rive'];
+  bot.loadFile(files, botLoaded, errorLoading);
+
+  // The bot is ready
+  function botLoaded() {
+    console.log("Bot loaded");
+    bot.sortReplies();
+  }
+
+  // There was a problem
+  function errorLoading(error) {
+    console.log("Error when loading rivescript files: " + error);
+  }
+
+  // Grab some DOM elements
+  var button = select('#submit');
+  var input = select('#textinput');
+  var output = select('#bot');
+
+  // Whenever the user hits "submit" ask the bot to say something
+  button.mousePressed(chat);
+
+
+  function chat() {
+    // What did the user say?
+    var txt = input.value();
+    // What does the bot say?
+   reply = bot.reply("local-user", txt);
+    // show the reply
+  //  output.html(reply);
+  speech.speak(reply);
+  }
+
+}
