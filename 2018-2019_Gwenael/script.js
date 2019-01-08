ebb010c029c6fddbaac94b791ce36de3fc56cf89
Julien <julien.drochon@gmail.com>
2019-01-08 12:07:38 +0100

    put comments

--- 2018-2019_Gwenael/script.js
+++ 2018-2019_Gwenael/script.js
@@ -1,21 +1,20 @@
-// Wait for the page to be ready
+// 3 couleurs par défaut au démarrage, avant qu'on les change
 var colors =[{r: 255, g: 0, b: 0}, {r: 0, g: 255, b: 0}, {r: 0, g: 0, b: 255}];
 
-
+// au chargement de la page…
 window.addEventListener("load", function(e) {
-
-  // console.log("Page loaded!");
-
-  // Grab reference to the tags we will be using
+  // on déclare les variables DOM de la page HTML
   var slider0 = document.getElementById("tolerance0");
   var slider1 = document.getElementById("tolerance1");
   var slider2 = document.getElementById("tolerance2");
+  // variable DOM pour le canvas
   var canvas  = document.getElementById('canvas');
 
   var context = canvas.getContext('2d');
   var webcam = document.getElementById('webcam');
   var clickedcolor = document.getElementById('pickedupcolor');
 
+  // pour les 3 couleurs de départ (toutes celles stockées dans l'array colors)…
   for(var i = 0; i < colors.length;i++)
   {
     document.getElementById('color'+i).style.backgroundColor = "rgb("+colors[i].r+","+colors[i].g+","+colors[i].b+")";
@@ -24,8 +23,7 @@ window.addEventListener("load", function(e) {
     document.getElementById("number"+i+"blue").value = colors[i].b;
   }
 
-
-
+  // fonctions pour ajuster la sensibilité des couleurs choisies
   tracking.ColorTracker.registerColor('0', function(r, g, b) {
     return getColorDistance(colors[0], {r: r, g: g, b: b}) < slider0.value;
   });
@@ -36,31 +34,29 @@ window.addEventListener("load", function(e) {
     return getColorDistance(colors[2], {r: r, g: g, b: b}) < slider2.value
   });
 
+  // stockage des couleurs choisies dans le tracker
   var tracker = new tracking.ColorTracker(["0","1","2"]);
 
-  // Add callback for the "track" event
+  // Affichage des rectangles autour des formes colorées
   tracker.on('track', function(e) {
-
     context.clearRect(0, 0, canvas.width, canvas.height);
-    // alert(e.data.length);
     if (e.data.length !== 0) {
-
       e.data.forEach(function(rect) {
-
         console.log(colors[parseInt(rect.color)]);
         drawRect(rect, context, colors[parseInt(rect.color)]);
-        //  drawRect(rect, context, colors[1]);
-        //  drawRect(rect, context, colors[2]);
+        // collision
+        // if (rect1.x < rect2.x + rect2.width &&
+        //    rect1.x + rect1.width > rect2.x &&
+        //    rect1.y < rect2.y + rect2.height &&
+        //    rect1.height + rect1.y > rect2.y) {
+        //     // collision détectée !
+        // }
       });
-
     }
-
   });
 
-  // Start tracking
-  tracking.track(
-    webcam,
-    tracker,
+  // Tracking video, taille de la capture de la fenetre video
+  tracking.track( webcam, tracker,
     {
       camera: true,  mediaConstraints: {
         video: {
@@ -72,13 +68,14 @@ window.addEventListener("load", function(e) {
     }
   );
 
+  // fonction pour obtenir les valeurs R, G, B quand on clique sur l'image de la webcam
   webcam.addEventListener("click", function (e) {
     var c = getColorAt(webcam, e.offsetX, e.offsetY);
     clickedcolor.innerHTML = "R : " + c.r + " G : " + c.g + " B : " + c.b;
   });
 
+  // fonction getColorAT
   function getColorAt(webcam, x, y) {
-
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
     canvas.width = webcam.width;
@@ -90,13 +87,13 @@ window.addEventListener("load", function(e) {
   }
 });
 
+// attribuer la couleur au rectangle situé au dessus des champs de valeurs RGB, une fois qu'on a cliqué sur le bouton ok
 function submitColor(index){
   colors[index]= {r: document.getElementById("number"+index+"red").value, g: document.getElementById("number"+index+"green").value, b: document.getElementById("number"+index+"blue").value};
   document.getElementById('color'+index).style.backgroundColor = "rgb("+colors[index].r+","+colors[index].g+","+colors[index].b+")";
-  //  console.log(document.getElementById("number"+index+"red").value);
-  //  console.log(colors[index].r);
 }
 
+// function pour ajuster la couleur avec les sliders
 function getColorDistance(target, actual) {
   return Math.sqrt(
     (target.r - actual.r) * (target.r - actual.r) +
@@ -105,9 +102,9 @@ function getColorDistance(target, actual) {
   );
 }
 
+// Deesin des rectangles autour de la forme colorée
 function drawRect(rect, context, kolor) {
-  // context.strokeStyle = "rgb("+ kolor.r + "," + kolor.g + "," + kolor.b ")" ;
-  context.strokeStyle = "rgb("+parseInt(kolor.r)+","+parseInt(kolor.g)+","+parseInt(kolor.b)+")" ;
-  context.strokeWeight = "1 px";
+  context.strokeStyle = "rgb("+parseInt(kolor.r)+","+parseInt(kolor.g)+","+parseInt(kolor.b)+")" ; // couleur du contour
+  context.strokeWeight = "1 px"; //épaisseur du contour
   context.strokeRect(rect.x, rect.y, rect.width, rect.height);
 }
