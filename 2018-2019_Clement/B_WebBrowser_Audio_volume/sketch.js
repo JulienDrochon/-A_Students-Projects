98f3010a9d259b0cf9bc2ae032e984f27b3b58bd
Julien <julien.drochon@gmail.com>
2019-01-08 10:13:00 +0100

    change code

--- 2018-2019_Clement/B_WebBrowser_Audio_volume/sketch.js
+++ 2018-2019_Clement/B_WebBrowser_Audio_volume/sketch.js
@@ -4,7 +4,27 @@
 // License Creative Commons BY-NC
 
 
-jQuery(document).ready(function($) {
-foo = $("#controlAudio");
-foo.prop("volume", 0.5)
-});
+// jQuery(document).ready(function($) {
+// foo = $("#controlAudio");
+// foo.prop("volume", 0.5)
+// });
+
+var serial;          // variable to hold an instance of the serialport library
+var portName = '/dev/cu.usbmodemFD121'; // fill in your serial port name here
+var inputData;
+
+function setup() {
+  serial = new p5.SerialPort();    // make a new instance of the serialport library
+      serial.open(portName);
+}
+
+function draw() {
+console.log(inputData)
+}
+
+function serialEvent() {
+ // read a byte from the serial port:
+ var inByte = serial.read();
+ // store it in a global variable:
+ inputData = inByte;
+}
