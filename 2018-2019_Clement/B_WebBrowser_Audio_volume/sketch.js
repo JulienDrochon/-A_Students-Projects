e5ba06fb74caa653a75bd30d4cc779fb806d1e7a
Julien <julien.drochon@gmail.com>
2019-03-06 03:25:10 +0100

    new commit

--- 2018-2019_Clement/B_WebBrowser_Audio_volume/sketch.js
+++ 2018-2019_Clement/B_WebBrowser_Audio_volume/sketch.js
@@ -3,29 +3,90 @@
 //for Soft Machine Lesson at ESA Pyrenees : www.esapyrenees.fr
 // License Creative Commons BY-NC
 
-
-// jQuery(document).ready(function($) {
-// foo = $("#controlAudio");
-// foo.prop("volume", 0.5)
-// });
-
 var serial;          // variable to hold an instance of the serialport library
-var portName = '/dev/cu.usbmodemFD121'; // fill in your serial port name here
 var inputData;
+var soundFile01, soundFile02, soundFile03, soundFile04;
+// var soundEnd01 = false, soundEnd02 = false, soundEnd03= false, soundEnd04 = false;
+var trigger01;
+var isPlaying01 = false, isPlaying02 = false, isPlaying03 = false, isPlaying04 = false;
+// var soundFile;
+
+function preload(){
+  soundFile01 = loadSound("assets/1.mp3");
+  soundFile02 = loadSound("assets/2.mp3");
+}
+
 
 function setup() {
   serial = new p5.SerialPort();    // make a new instance of the serialport library
-      //serial.open(portName);
-      serial.on('data', serialEvent);
+  //serial.open(portName);
+  serial.on('data', serialEvent);
+
+  trigger01 = 1;
+
+  // soundFile = document.getElementById("controlAudio04");
+
+
+//  soundFile01 = document.getElementById("controlAudio01");
+  //soundFile01.play();
+  soundFile01.onended = function() {
+    //alert("The audio 1 has ended");
+    isPlaying01 = false;
+    trigger01 = 0;
+  };
+
+  soundFile02 = document.getElementById("controlAudio02");
+  soundFile02.onended = function() {
+    // alert("The audio 2 has ended");
+    isPlaying02 = false;
+    trigger01 = 2;
+  };
+  //
+  // soundFile03 = document.getElementById("controlAudio03");
+  // soundFile03.onended = function() {
+  //    // alert("The audio 3 has ended");
+  //   isPlaying03 = false;
+  //   trigger01 = 3;
+  // };
+  //
+  // soundFile04 = document.getElementById("controlAudio04");
+  // soundFile04.onended = function() {
+  //    // alert("The audio 4 has ended");
+  //   isPlaying04 = false;
+  //   trigger01 = 0;
+  // };
+
 }
 
 function draw() {
-console.log(inputData)
+  console.log(inputData, 'trigger01 : ' + trigger01, 'isPlaying01 : ' + isPlaying01, 'isPlaying02 : ' + isPlaying02, 'isPlaying03 : ' + isPlaying03);
 }
 
 function serialEvent() {
- // read a byte from the serial port:
- var inByte = serial.readLine();
- // store it in a global variable:
- inputData = inByte;
+  // read a byte from the serial port:
+  var inByte = serial.readLine();
+  // store it in a global variable:
+  inputData = inByte;
+  if (inputData < 940 ) {
+    if(trigger01 == 0 && isPlaying01 == false){
+      isPlaying01 = true;
+      soundFile01.play();
+    }
+    if(trigger01 == 1 && isPlaying02 == false){
+      isPlaying02 = true;
+      soundFile02.play();
+    }
+    if(trigger01 == 2 && isPlaying03 == false){
+      isPlaying03 = true;
+      soundFile03.play();
+    }
+    if(trigger01 == 3 && isPlaying04 == false){
+      isPlaying04 = true;
+      soundFile04.play();
+    }
+  }
+}
+
+function keyPressed() {
+
 }
---
+++ 2018-2019_Clement/C_WebBrowser_Audio_start/sketch.js
@@ -0,0 +1,49 @@
+var soundFile01, soundFile02, soundFile03, soundFile04;
+var serial;
+
+function preload() {
+  soundFile01 = loadSound('assets/1.mp3');
+  soundFile02 = loadSound('assets/2.mp3');
+  soundFile03 = loadSound('assets/3.mp3');
+  soundFile04 = loadSound('assets/4.mp3');
+}
+
+function setup() {
+  serial = new p5.SerialPort();
+  serial.open("/dev/cu.usbmodemFA131");
+  serial.on('data', serialEvent);
+
+  soundFile01.onended(aftersound1);
+  soundFile02.onended(aftersound2);
+  soundFile03.onended(aftersound3);
+}
+
+function serialEvent() {
+  let inString = serial.readLine();
+  trim(inString);  // on nettoie les données venant d'arduino
+  if (!inString) return; // on nettoie les données venant d'arduino
+
+  if(soundFile01.isPlaying() || soundFile02.isPlaying() || soundFile03.isPlaying() || soundFile04.isPlaying() )
+  {
+
+  }
+  else if (inString > 30)
+  {
+    soundFile01.play();
+  }
+}
+
+function aftersound1(){
+  console.log("fin son 1");
+  soundFile02.play();
+
+}
+
+function aftersound2(){
+  console.log("fin son 2");
+  soundFile03.play();
+}
+
+function aftersound3(){
+  console.log("fin son 3");
+}
